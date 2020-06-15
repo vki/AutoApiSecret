ad6cf610cc4aa858f332341b72255bc8daa8e531
howard <howard000@gmail.com>
2020-06-15 15:33:01 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -43,40 +43,23 @@ def main():
     'Content-Type':'application/json'
     }
     try:
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code == 200:
-            num1+=1
-            print("1调用成功"+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code == 200:
-            num1+=1
-            print("2调用成功"+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/drive/root',headers=headers).status_code == 200:
-            num1+=1
-            print('3调用成功'+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code == 200:
-            num1+=1
-            print('4调用成功'+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code == 200:
-            num1+=1
-            print('5调用成功'+str(num1)+'次')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',headers=headers).status_code == 200:
-            num1+=1
-            print('6调用成功'+str(num1)+'次')    
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/Inbox/messages/delta',headers=headers).status_code == 200:
-            num1+=1
-            print('7调用成功'+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children',headers=headers).status_code == 200:
-            num1+=1
-            print('8调用成功'+str(num1)+'次')
-        if req.get(r'https://api.powerbi.com/v1.0/myorg/apps',headers=headers).status_code == 200:
-            num1+=1
-            print('8调用成功'+str(num1)+'次') 
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders',headers=headers).status_code == 200:
-            num1+=1
-            print('9调用成功'+str(num1)+'次')
-        if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
-            num1+=1
-            print('10调用成功'+str(num1)+'次')
-            print('此次运行结束时间为 :', localtime)
+        status_code = req.get(r'https://graph.microsoft.com/v1.0/me/drive/root',headers=headers).status_code
+        
+        print("1 status_code"+status_code)
+        
+        status_code = req.get(r'https://graph.microsoft.com/v1.0/me/drive',headers=headers).status_code
+        
+        print("2 status_code "+status_code)
+        
+        status_code = req.get(r'https://graph.microsoft.com/v1.0/users ',headers=headers).status_code
+        
+        print("3 status_code "+status_code)
+        
+        status_code = req.get(r'https://graph.microsoft.com/v1.0/me/messages',headers=headers).status_code
+        
+        print("4 status_code "+status_code)
+
+        print('此次运行结束时间为 :', localtime)
     except:
         print("pass")
         pass
